37043645a3c6fd059553db178366ca67d55760ec
Takeshi Kunimi <takeshi.kunimi@umitron.com>
2021-09-16 01:06:14 +0900

    Ignore key events when the user is editing something.

--- content-script.js
+++ content-script.js
@@ -1,6 +1,11 @@
+// todo: Better extension name.
 // todo: Make keyboard shortcut and seconds configurable.
 
 document.addEventListener("keydown", (evt) =>{
+  let ae = document.activeElement
+  if (ae.isContentEditable || ae.tagName === 'INPUT' || ae.tagName === 'TEXTAREA') {
+    return
+  }
   if (evt.key === ';'){
     forwards();
   } else if (evt.key === 'h') {
